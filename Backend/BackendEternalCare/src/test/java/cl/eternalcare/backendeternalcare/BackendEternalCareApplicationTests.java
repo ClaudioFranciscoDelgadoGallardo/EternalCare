d4286f7b9d5c8c583b9c73a810a3422de7c64d12
Claudio Delgado <167589549+ClaudioFranciscoDelgadoGallardo@users.noreply.github.com>
2026-05-10 03:39:23 -0400

    se intenta arreglar el repositorio

---
+++ Backend/BackendEternalCare/src/test/java/cl/eternalcare/backendeternalcare/BackendEternalCareApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.eternalcare.backendeternalcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEternalCareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

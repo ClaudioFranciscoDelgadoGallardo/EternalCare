8acf585221021bc4bb1838c196a2dbec655e1a6a
Claudio Delgado <167589549+ClaudioFranciscoDelgadoGallardo@users.noreply.github.com>
2026-05-10 04:24:52 -0400

    se agregar microservicos a desarrollar en el proyecto

---
+++ Backend/BackendEternalCare/programacion-service/src/main/java/cl/eternalcare/programacionservice/ProgramacionServiceApplication.java
@@ -0,0 +1,13 @@
+package cl.eternalcare.programacionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramacionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramacionServiceApplication.class, args);
+	}
+
+}
